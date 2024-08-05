475583929050965424af186768b3a0f385d6f882
jtorova5 <jtorova5@gmail.com>
2024-08-05 07:23:03 -0500

    feat: creating the basic ManagerApp class

---
+++ Models/ManagerApp.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaC_sharp_JhonatanToro.Models;
+
+public class ManagerApp
+{
+
+}
