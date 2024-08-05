5065754464ec9caf5cf6f131f4bab9e23ec941e8
jtorova5 <jtorova5@gmail.com>
2024-08-05 07:02:03 -0500

    feat: adding the atributes and methods of the Animal's class

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -7,5 +7,35 @@ namespace PruebaC_sharp_JhonatanToro.Models;
 
 public class Animal
 {
+    protected int Id { get; set; }
+    protected string Name { get; set; }
+    protected DateOnly Birthdate { get; set; }
+    protected string Breed { get; set; }
+    protected string Color { get; set; }
+    protected double WeightInKg { get; set; }
 
+    public Animal(int id, string name, DateOnly birthdate, string breed, string color, double weightInKg)
+    {
+        Id = id;
+        Name = name;
+        Birthdate = birthdate;
+        Breed = breed;
+        Color = color;
+        WeightInKg = weightInKg;
+    }
+
+    public void ShowInformacion()
+    {
+
+    }
+
+    protected void BasicReview()
+    {
+
+    }
+
+    protected int CalculateAgeInMonths()
+    {
+        return 0;
+    }
 }
