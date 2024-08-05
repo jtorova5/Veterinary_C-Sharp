3e59183a9c2a908ba64f6ace68e17be6ad9d7f56
jtorova5 <jtorova5@gmail.com>
2024-08-05 07:06:53 -0500

    feat: adding the atributes and methods of the Dog's class

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -5,7 +5,30 @@ using System.Threading.Tasks;
 
 namespace PruebaC_sharp_JhonatanToro.Models;
 
-public class Dog
+public class Dog : Animal
 {
+    public bool BreedingStatus { get; set; }
+    public string Temperament { get; set; }
+    public string MicrochipNumber { get; set; }
+    public string BarkVolume { get; set; }
+    public string CoatType { get; set; }
 
+    public Dog(int id, string name, DateOnly birthdate, string breed, string color, double weightInKg, bool breedingStatus, string temperament, string microchipNumber, string barkVolume, string coatType) : base(id, name, birthdate, breed, color, weightInKg)
+    {
+        BreedingStatus = breedingStatus;
+        Temperament = temperament;
+        MicrochipNumber = microchipNumber;
+        BarkVolume = barkVolume;
+        CoatType = coatType;
+    }
+
+    public void CastrateAnimal()
+    {
+
+    }
+
+    public void Hairdress()
+    {
+        
+    }
 }
