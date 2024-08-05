dc35c77b8f0ab5f12ef3131751500e4dfc757f64
jtorova5 <jtorova5@gmail.com>
2024-08-05 07:22:02 -0500

    feat: adding the atributes and methods of the VeterinaryClinic class

--- Models/VeterinaryClinic.cs
+++ Models/VeterinaryClinic.cs
@@ -7,5 +7,66 @@ namespace PruebaC_sharp_JhonatanToro.Models;
 
 public class VeterinaryClinic
 {
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public List<Dog> Dogs { get; set; }
+    public List<Cat> Cats { get; set; }
 
+    public VeterinaryClinic()
+    {
+        Dogs = [];
+        Cats = [];
+    }
+    public VeterinaryClinic(string name, string address)
+    {
+        Name = name;
+        Address = address;
+        Dogs = [];
+        Cats = [];
+    }
+
+    public void SaveDog(Dog newDog)
+    {
+
+    }
+
+    public void SaveCat(Cat newCat)
+    {
+
+    }
+
+    public void UpdateDog(Dog dog)
+    {
+
+    }
+
+    public void UpdateCat(Cat cat)
+    {
+
+    }
+
+    public void DeleteDog(int id)
+    {
+
+    }
+
+    public void DeleteCat(int id)
+    {
+
+    }
+
+    public void ShowAllPatients()
+    {
+
+    }
+
+    public void ShowAnimal(string type)
+    {
+
+    }
+
+    public void ShowPatient(int idPatient)
+    {
+
+    }
 }
