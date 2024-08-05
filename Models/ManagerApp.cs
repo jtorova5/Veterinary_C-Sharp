e82f2cc9cf1672711050b2e585a7678883d76852
jtorova5 <jtorova5@gmail.com>
2024-08-05 07:26:56 -0500

    feat: adding the methods of the ManagerApp class

--- Models/ManagerApp.cs
+++ Models/ManagerApp.cs
@@ -5,7 +5,25 @@ using System.Threading.Tasks;
 
 namespace PruebaC_sharp_JhonatanToro.Models;
 
-public class ManagerApp
+public static class ManagerApp
 {
+    // public static Dog CreateDog()
+    // {
+    // }
 
+    // public static Cat CreateCat()
+    // {
+    // }
+
+    // public static void ShowHeader()
+    // {
+    // }
+
+    // public static void ShowFooter()
+    // {
+    // }
+
+    // public static void ShowSeparator()
+    // {
+    // }
 }
