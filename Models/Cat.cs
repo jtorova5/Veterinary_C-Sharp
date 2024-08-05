8259e7165b3d62c6fecf09b920e9da393c80a23c
jtorova5 <jtorova5@gmail.com>
2024-08-05 07:09:54 -0500

    feat: adding the atributes and methods of the Cat's class

--- Models/Cat.cs
+++ Models/Cat.cs
@@ -5,7 +5,24 @@ using System.Threading.Tasks;
 
 namespace PruebaC_sharp_JhonatanToro.Models;
 
-public class Cat
+public class Cat : Animal
 {
+    public bool BreedingStatus { get; set; }
+    public string FurLenght { get; set; }
 
+    public Cat(int id, string name, DateOnly birthdate, string breed, string color, double weightInKg, bool breedingStatus, string furLenght) : base(id, name, birthdate, breed, color, weightInKg)
+    {
+        BreedingStatus = breedingStatus;
+        FurLenght = furLenght;
+    }
+
+    public void CastrateAnimal()
+    {
+
+    }
+
+    public void Hairdress()
+    {
+        
+    }
 }
